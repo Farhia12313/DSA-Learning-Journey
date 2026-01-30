a1d5739ff25bada70b4fbc185dd74629e3c395b7
Sabiha Farhia Talukder <sabihatalukder6464@gmail.com>
2026-01-30 20:23:03 +0600

    Create MaxOfTwoNumber.cpp

---
+++ Basics/MaxOfTwoNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter two number:"<< endl;
+    cin >> a >> b;
+    if(a>b) cout <<"a is maximum";
+    else cout<<"b is maximum";
+    return 0;
+    
+}
