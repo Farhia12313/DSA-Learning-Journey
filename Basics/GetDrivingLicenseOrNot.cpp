932950ed14213f6a074a0b0759360e966889a9e7
Sabiha Farhia Talukder <sabihatalukder6464@gmail.com>
2026-01-30 21:00:58 +0600

    Create GetDrivingLicenseOrNot.cpp

---
+++ Basics/GetDrivingLicenseOrNot.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"Enter person age :";
+    cin >> n;
+    if(n >= 18) cout << "This person get driving license";
+    else cout <<"This person not get driving license";
+    return 0;
+}
