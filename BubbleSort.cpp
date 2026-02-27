e5f18de36ef2d0de7e6eca95d4b82acf35da0a5d
Sabiha Farhia Talukder <sabihatalukder6464@gmail.com>
2026-02-27 10:35:26 +0600

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubbleSort(vector<int>& arr){
+    int n = arr.size();
+    bool swapped;
+    for(int i=0;i<n-1;i++){
+        swapped = false;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped)
+        break;
+    }
+}
+void printVector(const vector<int>& arr){
+    for(int num: arr){
+        cout<<" "<<num;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    bubbleSort(arr);
+    cout << "Sorted array:" <<"\n";
+     printVector(arr);
+    return 0;
+}
